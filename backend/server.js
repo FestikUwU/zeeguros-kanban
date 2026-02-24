976a3ef2bba7a90a1a0ff89a4ebea82e3b2f5f33
georg <heorhii.tykhonov@cieep.com>
2026-02-24 09:31:27 +0100

    Fix port for Render

--- backend/server.js
+++ backend/server.js
@@ -85,4 +85,5 @@ app.delete("/tasks/:id", (req, res) => {
     );
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running on port " + PORT));
